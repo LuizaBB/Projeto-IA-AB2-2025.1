344872a51a7fa92c557ad08b04de31e176a60b49
LuizaBB <luizabezerrabastos@gmail.com>
2025-11-17 05:41:24 -0300

    Adição Gemini 1
    Criação da funcção "extract_dish+characteristics" que usa imput do usuário + prompt pré desenvolvido + esclarecimento de atributos para enviar ao Client (aqui sendo o Gemini) para extrair as características principais em um arquivo .json

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask import Flask, render_template, request, jsonify
 from google import genai
 from dotenv import load_dotenv
 
+from google.genai.errors import APIError
+import json
+
 load_dotenv()
 
 app = Flask(__name__)
@@ -20,15 +23,43 @@ def load_data():
         print(f"Erro ao carregar dados: Verifique se os arquivos CSV estão na pasta 'data'. Erro: {e}")
         exit()
 
+def extract_dish_characteristics(client: genai.Client, dish_description: str) -> dict:
+    prompt = f"""
+    Analise a descrição do prato abaixo e extraia as seguintes características em um objeto JSON: 
+    'tipo_carne' (ex: 'Carne Vermelha', 'Peixe', 'Aves', 'Vegetariano'),
+    'intensidade' (Um número de 1 a 5, onde 1 é muito leve e 5 é muito forte/encorpado),
+    'acidez' (ex: 'Baixa', 'Média', 'Alta'),
+    'sabor_principal' (ex: 'Doce', 'Salgado', 'Picante', 'Terroso').
+
+    Descrição do Prato: "{dish_description}"
+    """ #prompt detalhado para o gemini
+    try:
+        response = client.models.generate_content(model='gemini-2.5-flash', contents=prompt, config=genai.types.GenerateContentConfig(
+            response_mime_type="application/json", 
+                response_schema={
+                    "type": "object",
+                    "properties": {
+                        "tipo_carne": {"type": "string"},
+                        "intensidade": {"type": "number"},
+                        "acidez": {"type": "string"},
+                        "sabor_principal": {"type": "string"}}}))
+        return json.loads(response.text)
+    except APIError as e:
+        print(f"Erro na chamada da API Gemini: {e}")
+        return {}
+    except Exception as e:
+        print(f"Erro inesperado no processamento do JSON: {e}")
+        return {}
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         user_dish_description = request.form.get('dish_input')
         recommended_wine = "Aguardando Algoritmo..."
         justification = "Aguardando Justificativa do Gemini..."
-        return render_template('index.html', recommendation=recommended_wine, justification=justification,dish_input=user_dish_description)
-    return render_template('index.html')
+        return render_template('index1.html', recommendation=recommended_wine, justification=justification,dish_input=user_dish_description)
+    return render_template('index1.html')
 
 if __name__ == '__main__':
     load_data()
-    app.run(debug=True) #bom para desenolvimento, desativar em produção
+    app.run(debug=True) #bom para desenvolvimento, desativar em produção
